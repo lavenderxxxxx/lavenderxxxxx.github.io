42b6312b41550f3d393af0268cfbb502ed4a9d83
lavenderxxxxx <42161567+lavenderxxxxx@users.noreply.github.com>
2018-09-27 12:17:11 +0800

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -4,8 +4,8 @@
 var canvas = document.getElementsByTagName("canvas").item(0);
 var context = canvas.getContext("2d");
 
-canvas.width = document.body.getBoundingClientRect().width;//window.innerWidth;
-canvas.height = document.body.getBoundingClientRect().height;//window.innerHeight;
+canvas.width = window.innerWidth;//document.body.getBoundingClientRect().width;
+canvas.height =window.innerHeight;// document.body.getBoundingClientRect().height;
 
 context.globalCompositeOperation = 'lighter';
 
